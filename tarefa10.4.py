bb5fe8bea6cd33626b9a4e3ab778187bc4c4db67
Bruno <bruno.if@hotmail.com>
2023-10-08 23:22:00 -0300

    tarefa 10.4

--- tarefa10.4.py
+++ tarefa10.4.py
@@ -2,21 +2,24 @@
 
 resposta = []
 
-def palavrasString(textoCompleto):
+def palavrasString(textoCompleto): ##Separa todas as palavras e transforma em um vetor
     return textoCompleto.split(' ')
 
-def contar(textoCompleto, palavra):
+def contar(textoCompleto, palavra): ##Conta a quantidade de palavras que tem 
     return {
         'palavra': palavra,
         'repeticoes': textoCompleto.count(palavra)
     }
 
+def removerDuplicatas(vetor): ##Remove palavras duplicatas no vetor
+    return set (vetor)
 
 def main(textoCompleto):
     palavras = palavrasString(textoCompleto)
+    palavrasSemDuplicatas = removerDuplicatas(palavras)
 
-    for palavra in palavras:
+    for palavra in palavrasSemDuplicatas:
         resposta.append(contar(textoCompleto, palavra))
     return resposta
 
-print(main('um texto um de texto de dois'))
+print(main('Esse texto tem um texto grande com um parágrafo')) ##Retorna um dicionário com as chaves 'palavra' e 'repeticoes'
